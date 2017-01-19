f1c2d59c159e45a3a35dca3b7cba2168e00fce8b
Martin Müllenhaupt <mm+git@netlair.de>
2017-01-19 10:54:14 +0100

    format code

--- GPGNetMessage.ts
+++ GPGNetMessage.ts
@@ -1,107 +1,105 @@
 
 export class GPGNetMessage {
-    
-    public header : string;
-    public chunks : Array<any>;
-    
-    constructor(_header? : string, _chunks? : Array<any>) {
-        this.header = _header ? _header : 'uninitialized';
-        this.chunks = _chunks ? _chunks : new Array<any>();
+
+  public header: string;
+  public chunks: Array<any>;
+
+  constructor(_header?: string, _chunks?: Array<any>) {
+    this.header = _header ? _header : 'uninitialized';
+    this.chunks = _chunks ? _chunks : new Array<any>();
+  }
+
+  byteCount(): number {
+    let result: number = 0;
+    result += 4 /* headerSize */
+      + this.header.length /* header */
+      + 4;  /* chunksize */
+
+    for (let chunk of this.chunks) {
+      result += 1; /* typeCode */
+      if (typeof chunk === "string") {
+        result += (4 + chunk.length);
+      }
+      else {
+        result += 4;
+      }
     }
-    
-    byteCount() : number {
-        let result : number = 0;
-        result += 4 /* headerSize */
-               + this.header.length /* header */
-               + 4;  /* chunksize */
-        
-        for (let chunk of this.chunks) {
-            result += 1; /* typeCode */
-            if (typeof chunk === "string") {
-                result += (4 + chunk.length);
-            }
-            else {
-                result += 4;
-            }
+    return result;
+  }
+
+  toBuffer(): Buffer {
+    let result = Buffer.allocUnsafe(this.byteCount());
+    try {
+      let offset: number = 0;
+      offset = result.writeInt32LE(this.header.length, offset);
+      offset += result.write(this.header, offset, this.header.length, 'latin1');
+      offset = result.writeInt32LE(this.chunks.length, offset);
+
+      for (let chunk of this.chunks) {
+        if (typeof chunk === "string") {
+          offset = result.writeInt8(1, offset);
+          offset = result.writeInt32LE(chunk.length, offset);
+          offset += result.write(chunk, offset, chunk.length, 'latin1');
         }
-        return result;
+        else {
+          offset = result.writeInt8(0, offset);
+          offset = result.writeInt32LE(chunk, offset);
+        }
+      }
+    }
+    catch (e) {
+      console.error(e);
     }
 
-    toBuffer() : Buffer {
-        let result = Buffer.allocUnsafe(this.byteCount());
-        try {
-            let offset : number = 0;
-            offset = result.writeInt32LE(this.header.length, offset);
-            offset += result.write(this.header, offset, this.header.length, 'latin1'); 
-            offset = result.writeInt32LE(this.chunks.length, offset);
-            
-            for (let chunk of this.chunks) {
-                if (typeof chunk === "string") {
-                    offset = result.writeInt8(1, offset);
-                    offset = result.writeInt32LE(chunk.length, offset);
-                    offset += result.write(chunk, offset, chunk.length, 'latin1');
-                }
-                else {
-                    offset = result.writeInt8(0, offset);
-                    offset = result.writeInt32LE(chunk, offset);
-                }
-            }
+    return result;
+  }
+
+  static fromBuffer(buf: Buffer, callback: (msg: GPGNetMessage) => void): Buffer {
+    let result = Buffer.from(buf);
+
+    try {
+      while (true) {
+        let msg = new GPGNetMessage;
+        let offset: number = 0;
+        let headerLength = result.readInt32LE(offset);
+        //console.log(`parsing headerLength ${headerLength} offset ${offset}`);
+        offset += 4;
+        msg.header = result.toString('latin1', offset, offset + headerLength);
+        //console.log(`parsing header ${msg.header} offset ${offset}`);
+        offset += headerLength;
+        let chunkCount = result.readInt32LE(offset);
+        //console.log(`parsing chunkCount ${chunkCount} offset ${offset}`);
+        offset += 4;
+        for (let chunkIndex = 0; chunkIndex < chunkCount; chunkIndex++) {
+          //console.log(`parsing chunk ${chunkIndex}/${chunkCount}`);
+          let chunkType = result.readInt8(offset);
+          //console.log(`parsing chunkType ${chunkType} offset ${offset}`);
+          offset += 1;
+          switch (chunkType) {
+            case 0:
+              msg.chunks.push(result.readInt32LE(offset));
+              //console.log(`parsing non-string chunk ${msg.chunks[msg.chunks.length -1]} offset ${offset}`);
+              offset += 4;
+              break;
+            case 1:
+              let strlen = result.readInt32LE(offset);
+              //console.log(`parsing string of size ${strlen} offset %{offset}`);
+              offset += 4;
+              msg.chunks.push(result.toString('latin1', offset, offset + strlen));
+              //console.log(`parsing string chunk ${msg.chunks[msg.chunks.length -1]} offset ${offset}`);
+              offset += strlen;
+              break;
+
+          }
         }
-        catch(e)
-        {
-            console.error(e);
-        }
-        
-        return result;
+        result = result.slice(offset);
+        callback(msg);
+      }
     }
-    
-    static fromBuffer(buf : Buffer, callback: (msg : GPGNetMessage) => void ) : Buffer {
-        let result = Buffer.from(buf);
-        
-        try {
-            while(true) {
-                let msg = new GPGNetMessage;
-                let offset : number = 0;
-                let headerLength = result.readInt32LE(offset);
-                //console.log(`parsing headerLength ${headerLength} offset ${offset}`);
-                offset += 4;
-                msg.header = result.toString('latin1', offset, offset + headerLength);
-                //console.log(`parsing header ${msg.header} offset ${offset}`);
-                offset += headerLength;
-                let chunkCount = result.readInt32LE(offset);
-                //console.log(`parsing chunkCount ${chunkCount} offset ${offset}`);
-                offset += 4;
-                for(let chunkIndex = 0; chunkIndex < chunkCount; chunkIndex++) {
-                    //console.log(`parsing chunk ${chunkIndex}/${chunkCount}`);
-                    let chunkType = result.readInt8(offset);
-                    //console.log(`parsing chunkType ${chunkType} offset ${offset}`);
-                    offset += 1;
-                    switch(chunkType) {
-                        case 0:
-                            msg.chunks.push(result.readInt32LE(offset));
-                            //console.log(`parsing non-string chunk ${msg.chunks[msg.chunks.length -1]} offset ${offset}`);
-                            offset += 4;
-                            break;
-                        case 1:
-                            let strlen = result.readInt32LE(offset);
-                            //console.log(`parsing string of size ${strlen} offset %{offset}`);
-                            offset += 4;
-                            msg.chunks.push(result.toString('latin1', offset, offset + strlen));
-                            //console.log(`parsing string chunk ${msg.chunks[msg.chunks.length -1]} offset ${offset}`);
-                            offset += strlen;
-                            break;
-                            
-                    }
-                }
-                result = result.slice(offset);
-                callback(msg);
-            }
-        }
-        catch(e) /* TODO: catch only RangeError exceptions */
-        {
-            //console.log(e);
-        }
-        
-        return result;
+    catch (e) /* TODO: catch only RangeError exceptions */ {
+      //console.log(e);
     }
+
+    return result;
+  }
 }
--- options.ts
+++ options.ts
@@ -1,27 +1,27 @@
 import * as optimist from 'optimist';
 
 export let options = optimist
-    .usage('Usage: $0 --id [num] --login [string]')
-    .demand(['id','login'])
-    .describe('id', 'set the ID of the local player')
-    .describe('login', 'set the login of the local player, e.g. "Rhiza"')
-    .default('rpc_port', 7236)
-    .describe('rpc_port', 'set the port of internal JSON-RPC server')
-    .default('gpgnet_port', 7237)
-    .describe('gpgnet_port', 'set the port of internal GPGNet server')
-    .default('lobby_port', 7238)
-    .describe('lobby_port', 'set the port the game lobby should use for incoming UDP packets from the PeerRelay')
-    .default('stun_server', 'dev.faforever.com')
-    .describe('stun_server', 'set the STUN server hostname')
-    .default('turn_server', 'dev.faforever.com')
-    .describe('turn_server', 'set the TURN server hostname')
-    .default('turn_user', '')
-    .describe('turn_user', 'set the TURN username')
-    .default('turn_pass', '')
-    .describe('turn_pass', 'set the TURN password')
-    .default('log_file', '')
-    .describe('log_file', 'set a verbose log file')
-    .argv;
+  .usage('Usage: $0 --id [num] --login [string]')
+  .demand(['id', 'login'])
+  .describe('id', 'set the ID of the local player')
+  .describe('login', 'set the login of the local player, e.g. "Rhiza"')
+  .default('rpc_port', 7236)
+  .describe('rpc_port', 'set the port of internal JSON-RPC server')
+  .default('gpgnet_port', 7237)
+  .describe('gpgnet_port', 'set the port of internal GPGNet server')
+  .default('lobby_port', 7238)
+  .describe('lobby_port', 'set the port the game lobby should use for incoming UDP packets from the PeerRelay')
+  .default('stun_server', 'dev.faforever.com')
+  .describe('stun_server', 'set the STUN server hostname')
+  .default('turn_server', 'dev.faforever.com')
+  .describe('turn_server', 'set the TURN server hostname')
+  .default('turn_user', '')
+  .describe('turn_user', 'set the TURN username')
+  .default('turn_pass', '')
+  .describe('turn_pass', 'set the TURN password')
+  .default('log_file', '')
+  .describe('log_file', 'set a verbose log file')
+  .argv;
 
 export default options;
 
