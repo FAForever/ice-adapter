bf23f2b7238fa11aa8bb2465548aeabbcc071d69
Martin Müllenhaupt <mm+git@netlair.de>
2017-01-19 10:54:14 +0100

    format code

--- GPGNetServer.ts
+++ GPGNetServer.ts
@@ -1,44 +1,44 @@
-import {createServer, Socket, Server} from 'net';
+import { createServer, Socket, Server } from 'net';
 import options from './options';
-import {GPGNetMessage} from './GPGNetMessage';
+import { GPGNetMessage } from './GPGNetMessage';
 import * as winston from 'winston';
 
 export class GPGNetServer {
-    
-    public server : Server;
-    public socket : Socket;
-    
-    buffer : Buffer;
-    
-    constructor(callback: (msg : GPGNetMessage) => void ) {
-        
-        this.buffer = Buffer.alloc(0);
-        
-        this.server = createServer( (socket) => {
-            winston.info('GPGNet client connected');
-            this.socket = socket;
-            this.socket.on('data', (data : Buffer) => {
-                //console.log(`GPGNet server received ${data.toString('hex')}`);
-                this.buffer = Buffer.concat([this.buffer, data], this.buffer.length + data.length);
-                this.buffer = GPGNetMessage.fromBuffer(this.buffer, callback);
-            });
-            
-            this.socket.on('close', (had_error) => {
-                winston.info('GPGNet client disconnected');
-            });
-        }).listen(options.gpgnet_port, 'localhost', () => {
-            winston.info(`GPGNet server listening on port ${this.server.address().port}`);
-        });
-    }
-    
-    send(msg : GPGNetMessage) {
-        let bufToSend : Buffer = msg.toBuffer();
-        /*console.log(`GPGNet server sending ${bufToSend.toString('hex')}`);
-        GPGNetMessage.fromBuffer(bufToSend, (msg : GPGNetMessage) => {
-            console.log(`GPGNet server sending ${msg}`);
-        });
-        */
-        this.socket.write(bufToSend);
-    }
-    
+
+  public server: Server;
+  public socket: Socket;
+
+  buffer: Buffer;
+
+  constructor(callback: (msg: GPGNetMessage) => void) {
+
+    this.buffer = Buffer.alloc(0);
+
+    this.server = createServer((socket) => {
+      winston.info('GPGNet client connected');
+      this.socket = socket;
+      this.socket.on('data', (data: Buffer) => {
+        //console.log(`GPGNet server received ${data.toString('hex')}`);
+        this.buffer = Buffer.concat([this.buffer, data], this.buffer.length + data.length);
+        this.buffer = GPGNetMessage.fromBuffer(this.buffer, callback);
+      });
+
+      this.socket.on('close', (had_error) => {
+        winston.info('GPGNet client disconnected');
+      });
+    }).listen(options.gpgnet_port, 'localhost', () => {
+      winston.info(`GPGNet server listening on port ${this.server.address().port}`);
+    });
+  }
+
+  send(msg: GPGNetMessage) {
+    let bufToSend: Buffer = msg.toBuffer();
+    /*console.log(`GPGNet server sending ${bufToSend.toString('hex')}`);
+    GPGNetMessage.fromBuffer(bufToSend, (msg : GPGNetMessage) => {
+        console.log(`GPGNet server sending ${msg}`);
+    });
+    */
+    this.socket.write(bufToSend);
+  }
+
 }
--- PeerRelay.ts
+++ PeerRelay.ts
@@ -1,35 +1,47 @@
 import { RTCPeerConnection, RTCSessionDescription, RTCIceCandidate, RTCDataChannel } from 'wrtc';
 import * as dgram from 'dgram';
 import options from './options';
+import * as winston from 'winston';
 
 class PeerRelay {
-    peerConnection : RTCPeerConnection;
-    localSocket : dgram.Socket;
-    localAddress : Object;
-    dataChannel : RTCDataChannel;
-    constructor(public login : string) {
-        this.peerConnection = new RTCPeerConnection();
-        this.localSocket = dgram.createSocket('udp4');
-        this.localSocket.on('listening', () => {
-          this.localAddress = this.localSocket.address();
-          console.log("PeerRelay for ${this.login} listening on ${address.address}:${address.port}");
-        });
-        
-        this.dataChannel = this.peerConnection.createDataChannel('faf');
-        this.dataChannel.onopen = () => {
-            this.dataChannel.log("PeerRelay for ${this.login}: data channel open");
-            this.dataChannel.onmessage = (event) => {
-              var data = event.data;
-              console.log("PeerRelay for ${this.login} received ${event.data}");
-              this.localSocket.send(event.data, options.lobby_port, "localhost");   
-            }
-        }
-        
-        this.localSocket.bind(0);
-        
-        this.localSocket.on('message', (msg, rinfo) => {
-          console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-          this.dataChannel.send(msg);
-        });
-   }
+  peerConnection: RTCPeerConnection;
+  localSocket: dgram.Socket;
+  localAddress: Object;
+  dataChannel: RTCDataChannel;
+  constructor(public remoteId: number,
+    public remoteLogin: string,
+    public createOffer: boolean) {
+
+    let iceServer = { urls: [`stun:${options.stun_server}`, `turn:${options.stun_server}`] };
+    if (options.turn_user != '') {
+      iceServer['username'] = options.turn_user;
+      iceServer['credential'] = options.turn_pass;
+    }
+
+    this.peerConnection = new RTCPeerConnection({
+      iceServers: [iceServer]
+    });
+    this.localSocket = dgram.createSocket('udp4');
+    this.localSocket.on('listening', () => {
+      this.localAddress = this.localSocket.address();
+      console.log("PeerRelay for ${this.login} listening on ${address.address}:${address.port}");
+    });
+    this.localSocket.bind(0);
+
+    this.dataChannel = this.peerConnection.createDataChannel('faf');
+    this.dataChannel.onopen = () => {
+      winston.info(`Relay for ${remotePlayerLogin}(${remotePlayerId}): data channel open`);
+      this.dataChannel.onmessage = (event) => {
+        var data = event.data;
+        console.log("PeerRelay for ${this.login} received ${event.data}");
+        this.localSocket.send(event.data, options.lobby_port, "localhost");
+      }
+    }
+
+
+    this.localSocket.on('message', (msg, rinfo) => {
+      console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+      this.dataChannel.send(msg);
+    });
+  }
 }
