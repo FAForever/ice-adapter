7a9609cd8def5d178f851825a6f4bcb0a9c93f56
Martin Müllenhaupt <mm+git@netlair.de>
2017-01-20 10:25:57 +0100

    first successful peerconnection in the testclient

---
+++ logger.ts
@@ -0,0 +1,21 @@
+
+import options from './options';
+import * as winston from 'winston';
+
+let transports = [new (winston.transports.Console)({
+  colorize: 'none',
+  level: 'debug',
+  timestamp: true,
+  debugStdout : true
+})];
+
+if (options.log_file.length > 0) {
+  transports.push(new (winston.transports.File)({
+    filename: options.log_file
+  }))
+};
+
+export let logger = new (winston.Logger)({
+  transports: transports
+});
+export default logger;
