9cff6528e87aa79112e799b9025cf5b9928f2eb9
Dragonfire <bastian616-git@yahoo.de>
2017-01-25 14:17:43 +0100

    Fix Paths

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import options from './options';
 import logger from './logger';
 
-logger.info(`Starting faf-ice-adapter version ${require('./package.json').version}`);
+logger.info(`Starting faf-ice-adapter version ${require('../package.json').version}`);
 
 import { IceAdapter } from './IceAdapter';
 
