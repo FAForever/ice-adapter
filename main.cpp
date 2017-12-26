5094602c98d31ff7b5b34545b9f7e4f94baf95b0
Martin Müllenhaupt <mm+git@netlair.de>
2017-12-26 23:18:27 +0100

    remove unix exception handler

--- main.cpp
+++ main.cpp
@@ -5,33 +5,8 @@
 #include "IceAdapterOptions.h"
 #include "logging.h"
 
-#if defined(WEBRTC_POSIX)
-#  include <stdio.h>
-#  include <execinfo.h>
-#  include <signal.h>
-#  include <stdlib.h>
-#  include <unistd.h>
-
-void exception_handler(int sig)
-{
-  void *array[32];
-  size_t size;
-
-  // get void*'s for all entries on the stack
-  size = backtrace(array, 32);
-
-  // print out all the frames to stderr
-  fprintf(stderr, "Error: signal %d:\n", sig);
-  backtrace_symbols_fd(array, size, STDERR_FILENO);
-  exit(1);
-}
-
-#endif
 int main(int argc, char *argv[])
 {
-#if defined(WEBRTC_POSIX)
-  signal(SIGSEGV, exception_handler);   // install our exception handler
-#endif
   auto options = faf::IceAdapterOptions::init(argc, argv);
 
   faf::logging_init(options.logLevel);
