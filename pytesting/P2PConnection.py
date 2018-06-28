3891cb68d91f9f8662bcff2ebff946de987e0207
Martin Müllenhaupt <mm+git@netlair.de>
2018-06-29 01:53:35 +0200

    work on python test

--- pytesting/P2PConnection.py
+++ pytesting/P2PConnection.py
@@ -10,14 +10,24 @@ class P2PConnection(object):
     'pinging'
   ]
 
-  def __init__(self, client, peerId1, peerId2, hostId):
+  def __init__(self, client, peerId, remotePeerId, remotePeerLogin, hostId):
     self._client = client
     self._clientState = None
-    self._peer_id_1 = peerId1
-    self._peer_id_2 = peerId2
-    self._host_id = hostId
+    self._peerId = peerId
+    self._remotePeerId = remotePeerId
+    self._remotePeerLogin = remotePeerLogin
+    self._hostId = hostId
     self._machine = Machine(model=self, states=P2PConnection.states, initial='initial')
     self._machine.add_transition(trigger='connect_sent', source='initial', dest='connecting', after='showstate')
 
+    if peerId == hostId:
+      client.call("sendToIceAdapter", ["connectToPeer", [remotePeerLogin, remotePeerId, True]])
+    elif remotePeerId != hostId:
+      client.call("sendToIceAdapter", ["connectToPeer", [remotePeerLogin, remotePeerId, peerId < remotePeerId]])
+
   def showstate(self):
     self._client.log("P2PConnection adapter state changed to {}".format(self.state))
+
+  def onIceMessage(self, message):
+    self._client.call("sendToIceAdapter", ["iceMsg", [self._remotePeerId, message]])
+
